a55d2ef9e34b7612d55377e4c918a4e4b9948a36
Rishabh Pathak <clusmsyknight@gmail.com>
2021-04-18 15:40:35 +0530

    like/dislike post

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -46,5 +46,23 @@ module.exports = {
         throw new UserInputError("Post not found");
       }
     },
+    likePost: async (_, { postId }, context) => {
+      const { username } = checkAuth(context);
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          // Post already liked, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          // Not liked, like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
+    },
   },
 };
