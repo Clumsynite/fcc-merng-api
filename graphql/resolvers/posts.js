bb0065b0579fa25adf3e81d7b5d9391e2eaa8607
Rishabh Pathak <clusmsyknight@gmail.com>
2021-04-18 15:07:12 +0530

    delete post
    check OP is same as user

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require("apollo-server");
+
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/checkAuth");
 
@@ -37,6 +39,20 @@ module.exports = {
 
       const post = await newPost.save();
       return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return "Post deleted Successfully";
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } catch (error) {
+        throw new Error(error);
+      }
     },
   },
 };
