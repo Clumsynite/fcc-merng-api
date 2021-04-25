d9b8bd660c4a34ab9dac97b448c84e506ddee36f
Rishabh Pathak <clusmsyknight@gmail.com>
2021-04-25 14:00:26 +0530

    fixed port

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ mongoose
   })
   .then(() => {
     console.log("MongoDB Connected");
-    return server.listen({ port: 5000 });
+    return server.listen({ port: process.env.PORT || 5000 });
   })
   .then((res) => console.log(`Server running at ${res.url}`));
